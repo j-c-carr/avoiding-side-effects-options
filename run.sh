b2f98f5b1e47b73d5508054c34aa4107fd97d2d8
jonathan <jonathancolacocarr@gmail.com>
2024-07-23 18:55:31 -0400

    Test 8 envs

--- run.sh
+++ run.sh
@@ -6,12 +6,14 @@ set -x  # for printing commands
 
 current_datetime=$(date '+%Y-%m-%d-%H-%M')
 
-for seed in {1..1}
+for seed in {2..2}
 do
   # Save a video of the agent on the first run
   python3 train.py \
       --wandb \
       --project="safelife-debug" \
-      --n_envs=1 \
-      --name="test-1-env"
+      --n_envs=8 \
+      --name="test-1-env" \
+      --seed=$seed \
+      --log_dir=logs/seed_test_$current_datetime
 done
